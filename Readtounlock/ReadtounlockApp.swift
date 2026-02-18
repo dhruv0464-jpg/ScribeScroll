dafd775be3f8bc962a9c87ae0e9ec4bba3e8cd81
dhruv bhatia <dhruv0464@gmail.com>
2026-02-18 13:28:35 -0600

    Initial Commit

---
+++ Readtounlock/ReadtounlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadtounlockApp.swift
+//  Readtounlock
+//
+//  Created by Dhruv Bhatia on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadtounlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
